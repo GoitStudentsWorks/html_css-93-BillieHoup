5cb49e2f1d2fcd4149f8914082c88b8090ae16a9
NepalMan <126617715+NepalMan@users.noreply.github.com>
2023-08-15 23:04:24 +0300

    header-done

--- src/js/mob-menu.js
+++ src/js/mob-menu.js
@@ -1,25 +1,52 @@
-(() => {
-    const refs = {
-      openMenuBtn: document.querySelector("[data-menu-open]"),
-      closeMenuBtn: document.querySelector("[data-menu-close]"),
-      menu: document.querySelector("[data-menu]"),
-    };
+// (() => {
+//     const refs = {
+//       openMenuBtn: document.querySelector("[data-menu-open]"),
+//       closeMenuBtn: document.querySelector("[data-menu-close]"),
+//       menu: document.querySelector("[data-menu]"),
+//     };
   
-    refs.openMenuBtn.addEventListener("click", toggleMenu);
-    refs.closeMenuBtn.addEventListener("click", toggleMenu);
+//     refs.openMenuBtn.addEventListener("click", toggleMenu);
+//     refs.closeMenuBtn.addEventListener("click", toggleMenu);
   
-    function toggleMenu() {
-  const isOpen = !refs.menu.classList.contains("is-hidden");
+//     function toggleMenu() {
+//   const isOpen = !refs.menu.classList.contains("is-hidden");
 
-  if (isOpen) {
-    refs.menu.style.left = "-900px"; 
-    document.body.classList.remove("no-scroll");
-  } else {
-    refs.menu.style.left = "0"; 
-    document.body.classList.add("no-scroll");
-  }
+//   if (isOpen) {
+//     refs.menu.style.left = "-900px"; 
+//     document.body.classList.remove("no-scroll");
+//   } else {
+//     refs.menu.style.left = "0"; 
+//     document.body.classList.add("no-scroll");
+//   }
 
-  refs.menu.classList.toggle("is-hidden");
-}
-  })();
-  
+//   refs.menu.classList.toggle("is-hidden");
+// }
+//   })();
+
+document.addEventListener("DOMContentLoaded", function() {
+  const mobMenuOpenButtons = document.querySelectorAll("[data-menu-open]");
+  const mobMenuCloseButtons = document.querySelectorAll("[data-menu-close]");
+  const mobMenu = document.querySelector("[data-menu]");
+
+  mobMenuOpenButtons.forEach(button => {
+    button.addEventListener("click", function() {
+      mobMenu.classList.remove("is-hidden");
+    });
+  });
+
+  mobMenuCloseButtons.forEach(button => {
+    button.addEventListener("click", function() {
+      mobMenu.classList.add("is-hidden");
+    });
+  });
+
+  const anchorLinks = document.querySelectorAll(".mob-menu-item a");
+  anchorLinks.forEach(link => {
+    link.addEventListener("click", function() {
+      mobMenu.classList.add("is-hidden");
+    });
+  });
+});
+
+
+
